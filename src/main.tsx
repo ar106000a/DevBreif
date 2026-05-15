5c6940dd28bcdee1bcdb7ccd4bb6bd9d3506eed0
ar1062000 <ar1062000a@gmail.com>
2026-05-15 21:32:32 +0500

    route fixing the relogin issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./index.css";
 import Landing from "./pages/Landing";
 import AppPage from "./pages/App";
 import Auth from "./pages/Auth";
 
+const isLoggedIn = !!localStorage.getItem("token");
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route
+          path="/"
+          element={isLoggedIn ? <Navigate to="/app" replace /> : <Landing />}
+        />
         <Route path="/app" element={<AppPage />} />
         <Route path="/auth" element={<Auth />} />
       </Routes>
